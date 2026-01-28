917f252a713fed68b84995f0cbe8c3f437bf44b1
Yash Raghubanshi <yashraghubanshi@Yashs-MacBook-Pro-3.local>
2026-01-28 15:25:14 +0530

    fix issue in package.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve()
 
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174", process.env.CLIENT_URL],
     credentials: true,
 }))
 
