9cf51d0acfc56295be75c8aeac20c3cdcd826edb
Yash Raghubanshi <yashraghubanshi@Yashs-MacBook-Pro-3.local>
2026-01-29 00:40:41 +0530

    fix(backend): export streamClient from lib/stream.js

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,31 +1,31 @@
-import {StreamChat} from "stream-chat"
+import { StreamChat } from "stream-chat"
 import "dotenv/config";
 
-const apiKey= process.env.LINKUP_API_KEY
+const apiKey = process.env.LINKUP_API_KEY
 const apiSecret = process.env.LINKUP_API_SECRET
 
-if(!apiKey || !apiSecret){
+if (!apiKey || !apiSecret) {
     console.log("Stream API key or Secret is missing")
 }
 
-const streamClient = StreamChat.getInstance(apiKey,apiSecret)
+export const streamClient = StreamChat.getInstance(apiKey, apiSecret)
 
 export const upsertStreamUser = async (userData) => {
-    try{
+    try {
         await streamClient.upsertUsers([userData])
         return userData
     }
-    catch(error){
+    catch (error) {
         console.error("Error upserting Stream user:", error)
     }
 }
 
-export const generateStreamToken = (userId) =>{
-    try{
+export const generateStreamToken = (userId) => {
+    try {
         const userIdStr = userId.toString()
         return streamClient.createToken(userIdStr)
     }
-    catch(error){
+    catch (error) {
         console.error("Error generating Stream token:", error)
     }
 }
